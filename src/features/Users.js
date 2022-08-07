261d72ed4c39f6f679f0a1ef1162f2b581892c93
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-08-07 14:23:10 +0530

    Initial Commit with Redux setup

---
+++ src/features/Users.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+    name: "users",
+    initialState: {value: []},
+    reducers: {
+        addUser: (state, action) => {
+            // Write code for adding a
+        }
+    }
+})
