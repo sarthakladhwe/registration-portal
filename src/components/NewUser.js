0dbc71251f6017cfa1092f4cfa496f3f872cedd4
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2022-08-09 22:26:54 +0530

    Form update

--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -16,23 +16,25 @@ function NewUser() {
 
   function formSubmit(e) {
     e.preventDefault();
-    console.log("New User:", userForm)
+    dispatch(addUser(userForm))
   }
 
   return (
     <div className='newuser-container'>
-      <h3>Fill in the details!</h3>
-      <form className='newuser-form' onSubmit={formSubmit}>
-          <label for="input-name">Name</label>
-          <input id="input-name" type="text" name='name' value={userForm.name} onInput={inputChange} />
-          <label for="input-emailId">Email Id</label>
-          <input id="input-emailId" type="email" name='email' value={userForm.email} onInput={inputChange} />
-          <label for="input-uid">UID</label>
-          <input id="input-uid" type="text" name='uid' value={userForm.uid} onInput={inputChange} />
-          <label for="input-description">Description</label>
-          <textarea id="input-description" type="text" name='description' value={userForm.description} onInput={inputChange} />
-          <button type='submit'>Submit</button>
-      </form>
+      <div>
+        <h3>Fill in the details!</h3>
+        <form className='newuser-form' onSubmit={formSubmit}>
+            <label for="input-name">Name</label>
+            <input id="input-name" type="text" name='name' value={userForm.name} onInput={inputChange} />
+            <label for="input-emailId">Email Id</label>
+            <input id="input-emailId" type="email" name='email' value={userForm.email} onInput={inputChange} />
+            <label for="input-uid">UID</label>
+            <input id="input-uid" type="text" name='uid' value={userForm.uid} onInput={inputChange} />
+            <label for="input-description">Description</label>
+            <textarea id="input-description" type="text" name='description' value={userForm.description} onInput={inputChange} />
+            <button type='submit'>Submit</button>
+        </form>
+      </div>
     </div>
   )
 }
